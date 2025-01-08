1c1b77d676d1e200b1164a7fa43519dbf240a82e
manjugdr <153181318+manjugdr@users.noreply.github.com>
2025-01-08 10:54:38 +0530

    Update variables.tf

--- ec2-instance/variables.tf
+++ ec2-instance/variables.tf
@@ -1,17 +1,17 @@
 variable "region" {
   description = "AWS region to deploy resources"
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "ami" {
-  description = "ami-053b12d3152c0cc71"
+  description = "AMI ID for the EC2 instance"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2"
-  default     = "t2.medium"
+  default     = "t2.micro"
 }
 
 variable "instance_name" {
-  description = "teragrunt-ec2-instance"
+  description = "Name tag for the EC2 instance"
 }
